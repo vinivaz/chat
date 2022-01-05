9286f829cb5e9eb598e298bada0f90bdb48dc23e
Vinicius Vaz <viniciusvaz.tech@gmail.com>
2022-01-05 19:59:32 -0300

    socket io cors updated

--- src/app/utils/websocket.js
+++ src/app/utils/websocket.js
@@ -66,7 +66,7 @@ function socketIo(app) {
   const { Server } = require('socket.io');
   const io = new Server(server, {
     cors: {
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
   });
